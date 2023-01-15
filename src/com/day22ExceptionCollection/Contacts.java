bdde2ffa9055414a68ec2e26739d1ec0874cb028
PAVAN903 <navap903@gmail.com>
2023-01-15 22:54:40 +0530

    check address

---
+++ src/com/day22ExceptionCollection/Contacts.java
@@ -0,0 +1,105 @@
+package com.day22ExceptionCollection;
+
+public class Contacts {
+	
+
+			private String fname;
+			private String mname;
+			private String lname;
+			private String city;
+			private String state;
+			private String zip;
+			private String mob_no;
+			private String mail;
+			 
+			public Contacts() {
+			}
+
+			public String getFname() {
+				return fname;
+			}
+
+			public void setFname(String fname) {
+				this.fname = fname;
+			}
+
+			public String getMname() {
+				return mname;
+			}
+
+			public void setMname(String mname) {
+				this.mname = mname;
+			}
+
+			public String getLname() {
+				return lname;
+			}
+
+			public void setLname(String lname) {
+				this.lname = lname;
+			}
+
+			public String getCity() {
+				return city;
+			}
+
+			public void setCity(String city) {
+				this.city = city;
+			}
+
+			public String getState() {
+				return state;
+			}
+
+			public void setState(String state) {
+				this.state = state;
+			}
+
+			public String getZip() {
+				return zip;
+			}
+
+			public void setZip(String zip) {
+				this.zip = zip;
+			}
+
+			public String getMob_no() {
+				return mob_no;
+			}
+
+			public void setMob_no(String mob_no) {
+				this.mob_no = mob_no;
+			}
+
+			public String getMail() {
+				return mail;
+			}
+
+			public void setMail(String mail) {
+				this.mail = mail;
+			}
+
+			
+			public Contacts(String fname, String mname, String lname, String city, String state, String zip, String mob_no,
+					String mail) {
+				super();
+				this.fname = fname;
+				this.mname = mname;
+				this.lname = lname;
+				this.city = city;
+				this.state = state;
+				this.zip = zip;
+				this.mob_no = mob_no;
+				this.mail = mail;
+			}
+			public String toString() {
+				return "\nfirstname:"+fname+"\nmiddlename:"+mname+"\nlastname:"+lname+"\ncity:"+city+"\nstate:"+state+"\nzip:"+zip+"\nmoobil,mail";
+				}
+			    }
+
+
+
+
+
+
+
---
+++ src/com/day22ExceptionCollection/Main.java
@@ -0,0 +1,37 @@
+package com.day22ExceptionCollection;
+
+import java.util.Scanner;
+
+public class Main {
+	 static Contacts contacts;
+	public void address() {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter first name");
+	String fname=sc.next();
+	System.out.println("enter midddle name");
+	String mname=sc.next();
+	System.out.println("enter last name");
+	String lname=sc.next();
+	System.out.println("enter city name");
+	String city=sc.next();
+	System.out.println("enter state name");
+	String state=sc.next();
+	System.out.println("enter zip name");
+	String zip=sc.next();
+	System.out.println("enter mobile number ");
+	String mob_no=sc.next();
+	System.out.println("enter mail id");
+	String mail=sc.next();
+	contacts=new Contacts(fname,mname,lname,city,state,zip,mob_no,mail);
+	}
+	  public static void main(String[] args) {
+		  System.out.println("welcome to address book program");
+		  Main addressbook=new Main();
+		  addressbook.address();
+		  System.out.println(contacts);
+		
+
+	
+	}
+}
+
